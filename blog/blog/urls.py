d15e5b7a3f9f8c09c6b15402749f4d70535de4ef
xianding <xianding0627@gamil.com>
2024-05-13 22:57:04 +0800

    '环境配置'

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+import logging
+from django.http import HttpResponse
+# 创建日志记录器
+logger = logging.getLogger('django')
+# 输出日志
+logger.debug('测试logging模块debug')
+logger.info('测试logging模块info')
+logger.error('测试logging模块error')
+def log(request):
+    return HttpResponse('test')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',log)
 ]
